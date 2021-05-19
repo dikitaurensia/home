83a4db9c71995d9cab9ed90b68ae201a59dd870e
taurens14 <diki.taurensia@gmail.com>
2021-05-19 11:56:31 +0700

    Updates

---
+++ precache-manifest.8ab0730e75161aa1031660f32ff2d6d0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bcff0c00b035a6d5689d49760ef7178",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d31c94c299d67b019b78",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "1390d13147fcc8767529",
+    "url": "/static/js/2.95a996af.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.95a996af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d31c94c299d67b019b78",
+    "url": "/static/js/main.e25b3c31.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "34c45ca1b0379a06d41d9bd90bde5afa",
+    "url": "/static/media/diki.34c45ca1.jpg"
+  },
+  {
+    "revision": "386bf153c0251e715c81fb1538319ff6",
+    "url": "/static/media/resume.386bf153.pdf"
+  }
+]);
