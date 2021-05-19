291ea2d4023684c2600e560de19a596370a52cdb
taurens14 <diki.taurensia@gmail.com>
2021-05-19 11:58:07 +0700

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,10 +12,10 @@ const Footer = (props) => {
         <a
           className="badge badge-dark"
           rel="noopener"
-          href="https://github.com/hashirshoaeb"
+          href="https://github.com/dikitaurensia"
           aria-label="My GitHub"
         >
-          Hashir Shoaib
+          Diki Taurens Sia
         </a>{" "}
         using <i className="fab fa-react" />
         <p>
